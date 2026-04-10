575f6145c8204525e4e37aab7ce7bc9a4ee63dde
priyam0025 <priyam98323@gmail.com>
2026-04-10 20:29:28 +0530

    fix: add express and dotenv config (refs #1)

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import dotenv from "dotenv"
+import { app } from "./app.js"
+
+dotenv.config({
+    path: "./.env"
+})
+
+app.on("error", (error) => {
+    console.log("ERROR: ", error);
+    throw error;
+})
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running at port ${process.env.PORT}`);
+})
