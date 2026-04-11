3660c8d1c5f18cb70f4f61207c07ddf3051df08c
priyam0025 <priyam98323@gmail.com>
2026-04-11 21:22:53 +0530

    feat: add winston logger with custom format (refs #3)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import dotenv from "dotenv"
 import { app } from "./app.js"
+import { createModuleLogger } from "../utils/logger.js";
+
+const log = createModuleLogger();
 
 dotenv.config({
     path: "./.env.development"
 })
 
 app.on("error", (error) => {
-    console.log("ERROR: ", error);
+    log.info("ERROR: ", error);
     throw error;
 })
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running at port ${process.env.PORT}`);
+    log.info(`Server is running at port ${process.env.PORT}`);
 })
