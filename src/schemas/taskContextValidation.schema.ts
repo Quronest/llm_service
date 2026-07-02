1ef2e8424e5584cb3ac6d91a444fdcf017750997
Sibsankar-de <sibsankarde910@gmail.com>
2026-07-02 18:10:01 +0530

    feat: Domain enum added and task generate schema fields case converted

--- src/schemas/taskContextValidation.schema.ts
+++ src/schemas/taskContextValidation.schema.ts
@@ -1,5 +1,7 @@
 import { extendZodWithOpenApi } from "@asteasolutions/zod-to-openapi";
 import z from "zod";
+import { domainEnumList } from "../enums/domain.enum";
+import { level, levelEnumList } from "../enums/level.enum";
 
 extendZodWithOpenApi(z);
 
@@ -11,12 +13,22 @@ export const taskContextValidationSchema = z.object({
     example:
       "Currently pursuing engineering degree with focus on web development and cloud technologies.",
   }),
-  domain: z.array(z.string()).openapi({
-    example: ["Web Development", "Cloud Computing", "DevOps"],
+  domain: z.enum(domainEnumList).openapi({
+    example: "Web Development",
+  }),
+  subdomains: z.array(z.string()).openapi({
+    example: [
+      "Frontend Development",
+      "Backend Development",
+      "Database Management",
+    ],
   }),
   tags: z.array(z.string()).openapi({
     example: [""],
   }),
+  level: z.enum(levelEnumList).openapi({
+    example: level.EASY
+  })
 });
 
 export type TaskContextValidationType = z.infer<
